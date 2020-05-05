4bd2dac392b05e5652c6674475205ae4cb2331fd
DucAnh <ducanhfo3888@gmail.com>
2020-05-05 10:35:45 +0700

    More product,add search by brand

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,12 +1,11 @@
 const connection = require('../db')
 module.exports.search = function(req,res){
     var q = req.query.q;
-    let sql = "SELECT * FROM `products` WHERE productName LIKE '%"+q+"%'"
+    let sql = "SELECT * FROM `products` WHERE productName LIKE '%"+q+"%' OR productBrand LIKE '%"+q+"%'"
    connection.query(sql,function(err,result){
        res.render('option/search',{
            products:result,
            searchFor:req.query.q
        })
-    //res.send(result)
    })
 }
